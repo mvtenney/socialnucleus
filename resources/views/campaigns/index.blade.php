d9710725766333a2f8d614edb262e8a75ca100f4
Michael Tenney <michaeltenney@live.com>
2020-11-29 14:51:52 -0600

    style updates

--- resources/views/campaigns/index.blade.php
+++ resources/views/campaigns/index.blade.php
@@ -17,12 +17,16 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="grid grid-cols-4 gap-4">
                 @forelse ($campaigns as $campaign)
-                    <div class="flex flex-col justify-start p-4 bg-white border border-gray-300 rounded-md b-2 hover:shadow-md hover:border-yellow-500">
-                        <h3 class="mb-6 font-bold capitalize text-md"><a href="{{ $campaign->path() }}">{{ $campaign->title }}</a></h3>
-                        <p>{{ $campaign->description }}</p>
+                    <div class="flex flex-col justify-between bg-white rounded-md shadow-md hover:shadow-lg">
+                        <div>
+                            <h3 class="px-4 mt-4 mb-2 text-lg font-normal capitalize border-l-4 border-yellow-500"><a href="{{ $campaign->path() }}">{{ str_limit($campaign->title, 60) }}</a></h3>
+                            <p class="px-4 mb-4 text-sm italic text-gray-300 border-l-4 border-white">{{ $campaign->owner->name }}</p>
+                            <p class="p-4 text-gray-400 ">{{ str_limit($campaign->description, 100) }}</p>
+                        </div>
+                        <a href="{{ $campaign->path() }}" class="self-start px-4 py-2 m-4 text-gray-500 border rounded-md border-grey-500 hover:bg-yellow-500 hover:text-white">View Campaign</a>
                     </div>
                 @empty
-                    <div class="w-full p-4 bg-white rounded shadow-lg">No Projects Yet.</div>
+                    <div class="w-full p-4 m-4 bg-white rounded shadow-lg">No Campaigns Yet.</div>
                 @endforelse
             </div>
         </div>
